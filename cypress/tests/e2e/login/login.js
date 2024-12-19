d75b488b8a76f64cf75f982822735983871573d0
Rafael Zanella <rafaelzanella17@gmail.com>
2024-12-19 17:06:26 -0300

    Criado casos de teste para logar no sistema Arquivos modificados - login.spec.js dentro da pasta e2e/main/ Arquivos modificados - login.js dentro da pasta e2e/login/
    Casos de testes - Should login successfully with valid credentials
    Casos de teste - Should show an error for invalid username
    Casos de teste - Should show an error for invalid password
    Casos de teste - Should show an error for empty username
    Casos de teste - Should show an error for empty password
    Casos de teste - Should show errors for empty password
    Casos de teste - Should show an error for a password below the minimum length

--- cypress/tests/e2e/login/login.js
+++ cypress/tests/e2e/login/login.js
@@ -9,9 +9,37 @@ class LoginPages {
   loginUser = (username, password) => {
     cy.get(this.usernameInput).type(username)
     cy.get(this.passwordInput).type(password)
-    cy.get(this.errorMensagemPassword).should('not.exist')
+    cy.get(this.errorMensagenUsername).should('not.exist')
     cy.get(this.errorMensagemPassword).should('not.exist')
     cy.get(this.submitButton).should('not.be.disabled').click()
+  }
+
+  loginUsernameEmpty = (password) => {
+    cy.get(this.passwordInput).type(password)
+    cy.get(this.errorMensagenUsername).should('exist')
+    cy.get(this.errorMensagemPassword).should('not.exist')
+    cy.get(this.submitButton).should('be.disabled')
+  }
+
+  loginPasswordEmpty = (username) => {
+    cy.get(this.usernameInput).type(username)
+    cy.get(this.errorMensagenUsername).should('not.exist')
+    cy.get(this.errorMensagemPassword).should('not.exist')
+    cy.get(this.submitButton).should('be.disabled')
+  }
+  
+  loginUsernamePasswordEmtpy = () => {
+    cy.get(this.usernameInput).click()
+    cy.get(this.passwordInput).click()
+    cy.get(this.errorMensagenUsername).should('exist')
+    cy.get(this.submitButton).should('be.disabled')
+  }
+
+  loginPasswordBelowMinimum = (username, password) => {
+    cy.get(this.usernameInput).type(username)
+    cy.get(this.passwordInput).type(password).blur()
+    cy.get(this.errorMensagenUsername).should('not.exist')
+    cy.get(this.submitButton).should('be.disabled')
   }
 }
 
--- cypress/tests/e2e/main/login.spec.js
+++ cypress/tests/e2e/main/login.spec.js
@@ -1,6 +1,6 @@
 // <reference types="cypress"/>
 import dataBase from '../../../../data/database.json'
-import LoginPages from '../login/login';
+import LoginPages from '../login/login'
 
 const loginPages = new LoginPages()
 
@@ -8,13 +8,39 @@ const { username } = dataBase.users[1]
 const senha = 's3cret'
 
 
-describe('Login com sucesso', () => {
+describe('Login Tests', () => {
 
   beforeEach(() => {
     cy.visit('/signin')
   })
-
-  it('Login com um usuário válido', () => {
+ 
+  it('Should login successfully with valid credentials', () => {
     loginPages.loginUser(username, senha)
-  });
+    cy.url().should('eq', 'http://localhost:3000/')
+  })
+
+  it('Should show an error for invalid username', () => {
+    loginPages.loginUser('maicon.mai', senha)
+  })
+
+  it('Should show an error for invalid password', () => {
+    loginPages.loginUser(username, 'mai@123')
+  })
+  
+  it('Should show an error for empty username', () => {
+    loginPages.loginUsernameEmpty(senha)
+  })
+  
+  it('Should show an error for empty password', () => {
+    loginPages.loginPasswordEmpty(username)
+  })  
+  
+  it('Should show errors for empty password', () => {
+    loginPages.loginUsernamePasswordEmtpy()
+  })  
+  
+  it('Should show an error for a password below the minimum length', () => {
+    loginPages.loginPasswordBelowMinimum(username, 'm@3')
+  })
+
 });
